177919101a995b95829ce2e16ee5f7e2d30c9342
Manohar <admin@Em-Ka-Book-2.local>
2019-07-19 15:08:19 +0530

    added Drawer componnet

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -3,8 +3,15 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import MenuIcon from "@material-ui/icons/Menu";
 import IconButton from "@material-ui/core/IconButton";
+import SideDrawer from './SideDrawer';
 
 class Header extends Component {
+  state = {
+    drawerOpen : false
+  }
+  toggleDrawer = (value) => {
+    
+  }
   render() {
     return (
       <AppBar
@@ -27,6 +34,9 @@ class Header extends Component {
           >
             <MenuIcon />
           </IconButton>
+          <SideDrawer 
+              open = {this.state.drawerOpen}
+              onClose = {(value) => this.toggleDrawer(value) } />
         </Toolbar>
       </AppBar>
     );
