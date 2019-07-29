9684bb912b38007d305472901eb3e53f946ee842
manoharb30 <manohar.hello@gmail.com>
2019-07-29 17:53:12 +0530

    added Carrousal component

--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { relative } from 'path';
 
 const Featured = () => {
     return (
-        <div>
+        <div style = {{position:relative}}>
+            <div className = "artist_name">
+                <div className = "wrapper" >
+                        Andrea 
+                </div>
+
+            </div>
             
         </div>
     );
